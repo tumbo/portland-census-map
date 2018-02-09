5ac1d1830ea5569e7207364169357740e853fcfb
twall <tomwall.col@gmail.com>
2018-02-08 17:27:52 -0800

    initial commit of repo

---
+++ README.md
@@ -0,0 +1,2 @@
+# portland-census-react-leaflet
+Portland Census Data Visualized Spatially with React.js and Leaflet
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './app.css';
+
+import Map from './map.jsx';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <Map />
+
+      </div>
+    );
+  }
+}
+
+export default App;
